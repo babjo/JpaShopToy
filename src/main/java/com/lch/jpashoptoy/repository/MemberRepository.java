b129cf52fd68b6b994b63eb809dae01d35f4c594
babjo <kd980311@naver.com>
2015-11-09 22:50:54 +0900

    회원가입 기능 완성

---
+++ src/main/java/com/lch/jpashoptoy/repository/MemberRepository.java
@@ -0,0 +1,26 @@
+package com.lch.jpashoptoy.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.lch.jpashoptoy.domain.Member;
+
+@Repository
+public class MemberRepository {
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	public void save(Member member){
+		em.persist(member);
+	}
+
+	public List<Member> findByName(String name) {
+		return em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList();
+	}
+
+}
---
+++ src/main/java/com/lch/jpashoptoy/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.lch.jpashoptoy.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lch.jpashoptoy.domain.Member;
+import com.lch.jpashoptoy.repository.MemberRepository;
+
+@Service
+@Transactional
+public class MemberService {
+	
+	@Autowired
+	MemberRepository memberRepository;
+	
+	
+	public Long join(Member member){
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
+	
+	private void validateDuplicateMember(Member member){
+		List<Member> findMembers = memberRepository.findByName(member.getName());
+		if(!findMembers.isEmpty())
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+	}
+
+}
